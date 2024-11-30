722b9ab6e76e17526bcda52401851c4cfd27a2ee
Anze10 <anze.repse@gmail.com>
2024-11-30 19:59:02 +0100

    luka je zlat

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,71 +1,90 @@
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import Google from "next-auth/providers/google";
-
-import { db } from "~/server/db";
-import {
-  accounts,
-  sessions,
-  users,
-  verificationTokens,
-} from "~/server/db/schema";
-
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
-declare module "next-auth" {
-  interface Session extends DefaultSession {
-    user: {
-      id: string;
-      // ...other properties
-      // role: UserRole;
-    } & DefaultSession["user"];
-  }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
-}
-
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
-export const authConfig = {
+import NextAuth, { type User } from "next-auth"
+import Google from "next-auth/providers/google"
+ 
+export const { handlers, auth } = NextAuth({
   providers: [
     Google({
+      // Google requires "offline" access_type to provide a `refresh_token`
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: { params: { access_type: "offline", prompt: "consent" } },
     }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
-  adapter: DrizzleAdapter(db, {
-    usersTable: users,
-    accountsTable: accounts,
-    sessionsTable: sessions,
-    verificationTokensTable: verificationTokens,
-  }),
   callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
+    async jwt({ token, account }) {
+      if (account) {
+        // First-time login, save the `access_token`, its expiry and the `refresh_token`
+        return {
+          ...token,
+          access_token: account.access_token,
+          expires_at: account.expires_at,
+          refresh_token: account.refresh_token,
+        }
+      } else if (Date.now() < (token as JWT).expires_at * 1000) {
+        // Subsequent logins, but the `access_token` is still valid
+        return token
+      } else {
+        // Subsequent logins, but the `access_token` has expired, try to refresh it
+        if (!token.refresh_token) throw new TypeError("Missing refresh_token")
+ 
+        try {
+          // The `token_endpoint` can be found in the provider's documentation. Or if they support OIDC,
+          // at their `/.well-known/openid-configuration` endpoint.
+          // i.e. https://accounts.google.com/.well-known/openid-configuration
+          const response = await fetch("https://oauth2.googleapis.com/token", {
+            method: "POST",
+            body: new URLSearchParams({
+              client_id: process.env.AUTH_GOOGLE_ID!,
+              client_secret: process.env.AUTH_GOOGLE_SECRET!,
+              grant_type: "refresh_token",
+              refresh_token: token.refresh_token!,
+            }),
+          })
+ 
+          const tokensOrError = await response.json()
+ 
+          if (!response.ok) throw tokensOrError
+ 
+          const newTokens = tokensOrError as {
+            access_token: string
+            expires_in: number
+            refresh_token?: string
+          }
+ 
+          token.access_token = newTokens.access_token
+          token.expires_at = Math.floor(
+            Date.now() / 1000 + newTokens.expires_in
+          )
+          // Some providers only issue refresh tokens once, so preserve if we did not get a new one
+          if (newTokens.refresh_token)
+            token.refresh_token = newTokens.refresh_token
+          return token
+        } catch (error) {
+          console.error("Error refreshing access_token", error)
+          // If we fail to refresh the token, return an error so we can handle it on the page
+          token.error = "RefreshTokenError"
+          return token
+        }
+      }
+    },
+    async session({ session, token }) {
+      session.error = token.error
+      return session
+    },
   },
-  
-} satisfies NextAuthConfig;
+})
+ 
+declare module "next-auth" {
+  interface Session {
+    error?: "RefreshTokenError"
+  }
+}
+ 
+declare module "next-auth/jwt" {
+  interface JWT {
+    access_token: string
+    expires_at: number
+    refresh_token?: string
+    error?: "RefreshTokenError"
+  }
+}
